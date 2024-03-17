341501a036b7315e6cdfb3ea3c4e3bc708529ce6
Navis Ayara <navisayara@gmail.com>
2024-03-17 17:10:32 +0300

    Added quiz View

--- router.py
+++ router.py
@@ -1,9 +1,11 @@
 import flet as ft
 from views.main_view import Home
 from views.chat_view import ChatView
+from views.quiz_view import QuizView
 
 def route_handler(page: ft.Page):
     return {
         "/": Home(page),
-        "/chat": ChatView(page)
+        "/chat": ChatView(page),
+        "/quiz": QuizView(page)
     }
