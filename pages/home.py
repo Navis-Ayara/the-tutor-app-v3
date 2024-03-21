f5727b2ef2a9a64b6cdca0e5e95635eb5f4d82eb
Navis Ayara <navisayara@gmail.com>
2024-03-21 20:47:16 +0300

    Changed from returning a view to an inherited class

--- pages/home.py
+++ pages/home.py
@@ -13,88 +13,91 @@ def print_all_sessions():
             session_id = file.split('_')[2].split('.')[0]
             print(f"Session ID: {session_id}")
 
-def _home(page: ft.Page):
-    def go_to_chat(e):
-        page.session.set("selected subject", e.control.content.value)
-        page.go("/chat")
 
-    subjects = [
-        "Mathematics",
-        "English",
-        "Kiswahili",
-        "Chemistry",
-        "Biology",
-        "Physics",
-        "History",
-        "Geography",
-        "Computer",
-        "French",
-        "Agriculture"
-    ]
+class HomeView(ft.UserControl):
+    def __init__(self, page: ft.Page):
+        super().__init__()
 
-    recent_sessions = []
+        self.page = page
+        self.subjects = [
+            "Mathematics",
+            "English",
+            "Kiswahili",
+            "Chemistry",
+            "Biology",
+            "Physics",
+            "History",
+            "Geography",
+            "Computer",
+            "French",
+            "Agriculture"
+        ]
+        self.recent_sessions = []
+        self.load_recent_sessions()
 
-    def load_chat(e):
-        page.session.set("session_id", e.control.content.value)
-        page.go("/chat")
+    def go_to_chat(self, e):
+        self.page.session.set("selected subject", e.control.content.value)
+        self.page.go("/chat")
 
-    chat_history_files = [f for f in os.listdir('data/history') if f.startswith('chat_history_')]
-    
-    for file in chat_history_files:
-        session_id = file.split('_')[2].split('.')[0]
-        recent_sessions.append(
-            ft.Container(
-                padding=5,
-                border_radius=5,
-                content=ft.Text(
-                    value=session_id
-                ),
-                on_click=load_chat
-            )
-        )
+    def load_chat(self, e=None):
+        self.page.session.set("session_id", e.control.content.value)
+        self.page.go("/chat")
 
-    return ft.Column([
-        ft.Container(
-            padding=10,
-            content=ft.Text(
-                value="Subjects",
-                size=18,
-                weight=ft.FontWeight.W_600
-            )
-        ),
-        ft.Row([
-            ft.Container(
-                width=200,
-                height=200,
-                border_radius=17,
-                bgcolor=ft.colors.SECONDARY_CONTAINER,
-                on_click=go_to_chat,
-                # image_src=images[i],
-                image_fit=ft.ImageFit.COVER,
-                content=ft.Text(
-                    value= i if i != "Mathematics"  else "",
-                    size=20,
-                    weight=ft.FontWeight.W_500
-                ),
-                alignment=ft.alignment.center
-            )
-            for i in subjects
-        ], scroll=ft.ScrollMode.ALWAYS),
-        ft.Container(
-            padding=10,
-            content=ft.Text(
-                value="Recent Sessions",
-                size=18,
-                weight=ft.FontWeight.W_600
-            )
-        ),
-        ft.ListView(
-            spacing=10,
-            auto_scroll=True,
-            controls=recent_sessions if recent_sessions else [
-                ft.Text(
-                    value="No recent sessions found"
+    def load_recent_sessions(self):
+        self.recent_sessions = []
+        chat_history_files = [f for f in os.listdir('data/history') if f.startswith('chat_history_')]
+
+        for file in chat_history_files:
+            session_id = file.split('_')[2].split('.')[0]
+            self.recent_sessions.append(
+                ft.Card(
+                    
                 )
-            ],
-        )
-    ])
+            )
+
+    def build(self):
+        return ft.Column([
+            ft.Container(
+                padding=10,
+                content=ft.Text(
+                    value="Subjects",
+                    size=18,
+                    weight=ft.FontWeight.W_600
+                )
+            ),
+            ft.Row([
+                ft.Container(
+                    width=200,
+                    height=200,
+                    border_radius=17,
+                    bgcolor=ft.colors.SECONDARY_CONTAINER,
+                    on_click=self.go_to_chat,
+                    # image_src=images[i],
+                    image_fit=ft.ImageFit.COVER,
+                    content=ft.Text(
+                        value=i if i != "Mathematics" else "",
+                        size=20,
+                        weight=ft.FontWeight.W_500
+                    ),
+                    alignment=ft.alignment.center
+                )
+                for i in self.subjects
+            ], scroll=ft.ScrollMode.ALWAYS),
+            ft.Container(
+                padding=10,
+                content=ft.Text(
+                    value="Recent Sessions",
+                    size=18,
+                    weight=ft.FontWeight.W_600
+                )
+            ),
+            ft.ListView(
+                spacing=10,
+                auto_scroll=True,
+                controls=self.recent_sessions if self.recent_sessions else [
+                    ft.Text(
+                        value="No recent sessions found"
+                    )
+                ],
+            )
+        ])
